32938941cb4eaad85450c1024f4c9d675ee191c9
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-12-27 21:37:56 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -138,8 +138,8 @@ if [ ! -d /opt/application/$APP_NAME ];then
           meteor add thereactivestack:blazetoreact
        fi
       if [ "$APP_TEMPLATE" == "jump-scroll-site" ];then
-          meteor npm install --save bcrypt babel-runtime
-        meteor add npm-bcrypt
+         meteor npm install --save bcrypt babel-runtime
+         meteor add npm-bcrypt
          meteor update
          meteor add orionjs:core twbs:bootstrap fortawesome:fontawesome orionjs:bootstrap iron:router
          meteor add orionjs:filesystem
