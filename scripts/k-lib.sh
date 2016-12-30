74e35616baeac44c2470ffd4601214c4008eee16
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2016-12-30 00:54:38 -0600

    Update k-lib.sh

--- scripts/k-lib.sh
+++ scripts/k-lib.sh
@@ -54,16 +54,8 @@
    }
 function k-colors {
 	colors_normal=(
-		root=white,lightgrey
+		root=black,white
 		border=white,white
-		checkbox=,black
-		entry=,black
-		label=black,
-		actlistbox=,black
-		helpline=,black
-		roottext=,black
-		emptyscale=black
-		disabledentry=black,
 		checkbox=,black
 		entry=,black
 		label=black,
