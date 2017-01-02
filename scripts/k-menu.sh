d5c60b984e0fb98d2d188d1d6cd5b3479a0fe8a9
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-01-01 23:15:53 -0600

    Update k-menu.sh

--- scripts/k-menu.sh
+++ scripts/k-menu.sh
@@ -75,7 +75,9 @@ else
 				;;
        				1) #select app
 					echo "select app"
-					selected_app=$(k-list-menu "$(k ls)" "Application" "Select default app for all meteor commands" "blue")
+					list_apps="$(k ls)"
+					echo $list_apps
+					selected_app=$(k-list-menu "$list_apps" "Application" "Select default app for all meteor commands" "blue")
 					if [ ! -z "$selected_app" ];then
 						k use $selected_app
 						cd /opt/application/$selected_app/app
