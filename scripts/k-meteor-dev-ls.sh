9313b9172e693ed5a5705d7ec2fd39d01be3cbc5
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-01-02 00:12:29 -0600

    Update k-meteor-dev-ls.sh

--- scripts/k-meteor-dev-ls.sh
+++ scripts/k-meteor-dev-ls.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-main() {
-#!/bin/bash
-      filelines=$(ls -d */ /opt/application | cut -f1 -d'/')
-      valid_apps=""
-      for line in $filelines ; do
-          #echo "Checking $line"
-          if [ -d /opt/application/$line/app/.meteor ] && [ ! "$line" == "_k-meteor-dev" ];then
-             echo $line
-          fi
-          
-      done
-      #echo $valid_apps
+filelines=$(ls -d */ /opt/application | cut -f1 -d'/')
 
-}
-main "$@"
+for line in $filelines ; do
+    #echo "Checking $line"
+    if [ -d /opt/application/$line/app/.meteor ] && [ ! "$line" == "_k-meteor-dev" ];then
+       echo $line
+    fi
+
+done
+
+
