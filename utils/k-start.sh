d1c7f2da389c7f488ee2d09bef38aa5609520da2
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-01-03 15:15:09 -0600

    Update k-start.sh

--- utils/k-start.sh
+++ utils/k-start.sh
@@ -3,7 +3,8 @@ docker stop k-meteor-dev
 docker rm k-meteor-dev
 mkdir -p /opt/application/
 docker volume create --name k-meteor-dev-local
+docker volume create --name k-meteor-dev-application
 docker build --tag="k-meteor-dev" git://github.com/dlintec/k-meteor-dev
 docker run -d --name k-meteor-dev \
---user root -p 80:80 -p 443:443 -v /opt/application://opt/application -v  k-meteor-dev-local://home/meteor --restart always k-meteor-dev
+--user root -p 80:80 -p 443:443 -v k-meteor-dev-application://opt/application -v  k-meteor-dev-local://home/meteor --restart always k-meteor-dev
 docker exec -it --user meteor k-meteor-dev /bin/bash
