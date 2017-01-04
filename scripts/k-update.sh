5747fb92395aaa5b9abc0734818b30482ae6dfc1
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-01-04 00:12:51 -0600

    Update k-update.sh

--- scripts/k-update.sh
+++ scripts/k-update.sh
@@ -5,10 +5,13 @@ cd $LOCAL_IMAGE_PATH
 git fetch --all
 git reset --hard origin/master
 git pull
+exitstatus=$?
+
 chmod -R +x $LOCAL_IMAGE_PATH/scripts/
 find $LOCAL_IMAGE_PATH/. -name "*.sh" | xargs chmod +x
 $LOCAL_IMAGE_PATH/scripts/k-registerscriptsfolder.sh
 $LOCAL_IMAGE_PATH/scripts/k-post-update.sh
+exit $exit_status
 }
 
 main "$@"
