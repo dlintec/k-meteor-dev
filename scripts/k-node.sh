280fe5ad7348905e09ca4c93a2a77ed17c502d18
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-21 16:11:30 -0500

    Update k-node.sh

--- scripts/k-node.sh
+++ scripts/k-node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm' >> $HOME/.bashrc
 source $NVM_DIR/nvm.sh
 
 nvm install node
