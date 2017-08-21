f29b61114a9979aecc9aa4360f5fe4f7c3f8ee75
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-21 15:56:05 -0500

    Update k-node.sh

--- scripts/k-node.sh
+++ scripts/k-node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash  
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | sh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 source $NVM_DIR/nvm.sh
 
