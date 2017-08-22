18faa26bb289b6b9d6b33189564155f3e7fb2371
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-21 21:57:38 -0500

    Update k-android.sh

--- scripts/k-android.sh
+++ scripts/k-android.sh
@@ -2,7 +2,8 @@
 if [ ! -d /home/meteor/Android ]; then
    wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
    unzip sdk-tools-linux-3859397 -d /home/meteor/android
-
+   echo 'export ANDROID_HOME=$HOME/android' >> ~/.bashrc
+   echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin' >> ~/.bashrc
 else
   echo "Android tools already installed"
 fi
