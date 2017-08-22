62c8c43e5ed97517e385f15b1a3fed11c1806ba5
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-21 21:15:35 -0500

    Update k-java.sh

--- scripts/k-java.sh
+++ scripts/k-java.sh
@@ -1,7 +1,9 @@
 #!/bin/bash  
-apt-get install android-sdk-platform-tools
+
 apt-get install software-properties-common
 add-apt-repository ppa:webupd8team/java
 apt-get update
 apt-get install -y oracle-java8-installer
-apt-get install android-sdk
+
+#apt-get install android-sdk
+#apt-get install android-sdk-platform-tools
