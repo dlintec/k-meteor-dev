9e3e18eb99c3d490fd2b5b17be633ed8b2eb0157
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-22 01:10:52 -0500

    Update k-android.sh

--- scripts/k-android.sh
+++ scripts/k-android.sh
@@ -1,4 +1,7 @@
 #!/bin/bash  
+source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
+current_app=$(kalan-var "CURRENT_APP")
+
 if  grep -q "ANDROID_HOME" $HOME/.bashrc; then
     echo "Android tools already installed"
 else
@@ -11,6 +14,7 @@ else
    
    wget https://dl.google.com/android/repository/tools_r25.2.3-linux.zip
    unzip tools_r25.2.3-linux -d /home/meteor/android25
+   rm -rf tools_r25.2.3-linux.zip
    echo 'export ANDROID_HOME=/home/meteor/android25' >> ~/.bashrc
    echo 'export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin' >> ~/.bashrc
    #touch ~/.android/repositories.cfg
@@ -21,5 +25,6 @@ else
    #sdkmanager 'ndk-bundle'
    #mkdir ~/.gradle
    #echo 'org.gradle.daemon=true' >> ~/.gradle/gradle.properties
+   
 fi
 
