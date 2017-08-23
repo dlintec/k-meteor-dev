70f314b53a33205a9fa2d5ebca6c3461b4f89000
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-23 11:19:37 -0500

    Update k-registerscript.sh

--- scripts/k-registerscript.sh
+++ scripts/k-registerscript.sh
@@ -6,7 +6,7 @@ nombrecompleto="${cadena##*/}"
 extension="${nombrecompleto##*.}"
 solonombre="${nombrecompleto%%.*}"
 chmod +x $cadena
-mkdir -p /home/meteor/links/
+
 ln -sf $LOCAL_IMAGE_PATH/scripts/$nombrecompleto /usr/local/bin/$solonombre
 ln -sf $LOCAL_IMAGE_PATH/scripts/$nombrecompleto /home/meteor/links/$solonombre
 
