282c455482c8d18063fdd757141ec18b2e957a28
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-23 09:54:31 -0500

    Update k-build.sh

--- scripts/k-build.sh
+++ scripts/k-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash  
 source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
 current_app=$(kalan-var "CURRENT_APP")
-echo "Starting Build for $current_app for http://$DOMAIN_NAME:80 ..."
-meteor build /opt/application/$current_app/build --server=http://$DOMAIN_NAME:80
+echo "Starting Build for $current_app for http://11.0.0.132:80 ..."
+meteor build /opt/application/$current_app/build --server=http://11.0.0.132:80
     
