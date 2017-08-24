1b6d2a297a5bb007ab6d7ab3c4d12e6a7d00fbd1
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-24 14:37:13 -0500

    Update k-build.sh

--- scripts/k-build.sh
+++ scripts/k-build.sh
@@ -2,18 +2,18 @@
 source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
 current_app=$(kalan-var "CURRENT_APP")
 server_url="$(kalan-var 'SERVER_URL')"
-mobile_url="$(kalan-var 'MOBILE_URL')"
-if [ -z "$mobile_url" ] ;then
-   mobile_url="$server_url"
-   kalan-var "MOBILE_URL" "$mobile_url"
+mobile_build_url="$(kalan-var 'MOBILE_BUILD_URL')"
+if [ -z "$mobile_build_url" ] ;then
+   mobile_build_url="$server_url"
+   kalan-var "MOBILE_BUILD_URL" "$mobile_build_url"
 fi
 
 
-echo "Building app [$current_app] MOBILE_URL=$mobile_url ..."
+echo "Building app [$current_app] MOBILE_BUILD_URL=$mobile_build_url ..."
 
 # modificar archivo de cordova para aceptar certificados autofirmados
 # cd /opt/application/android/app/.meteor/local/cordova-build/platforms/android/CordovaLib/src/org/apache/cordova/engine
 # /app/.meteor/local/cordova-build/platforms/android/CordovaLib/src/org/apache/cordova/engine/SystemWebViewClient.java
 
-meteor build /opt/application/$current_app/build --server=$mobile_url 
+meteor build /opt/application/$current_app/build --server=$mobile_build_url 
     
