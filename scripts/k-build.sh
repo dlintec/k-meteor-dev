97573423ae85a4f7266dcb1b529b620dbf09a713
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-23 21:48:16 -0500

    Update k-build.sh

--- scripts/k-build.sh
+++ scripts/k-build.sh
@@ -3,5 +3,10 @@ source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
 current_app=$(kalan-var "CURRENT_APP")
 server_url="$(kalan-var 'SERVER_URL')"
 echo "Starting Build for $current_app for $server_url ..."
+
+# modificar archivo de cordova para aceptar certificados autofirmados
+# cd /opt/application/android/app/.meteor/local/cordova-build/platforms/android/CordovaLib/src/org/apache/cordova/engine
+# /app/.meteor/local/cordova-build/platforms/android/CordovaLib/src/org/apache/cordova/engine/SystemWebViewClient.java
+
 meteor build /opt/application/$current_app/build --server=$server_url
     
