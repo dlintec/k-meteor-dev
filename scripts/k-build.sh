a4c3cbd138c0d433ecf35ac44aa1adce7d98ab03
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-24 22:06:00 -0500

    Update k-build.sh

--- scripts/k-build.sh
+++ scripts/k-build.sh
@@ -10,9 +10,11 @@ if [ -z "$mobile_build_url" ] ;then
 fi
 
 
-echo "Building app [$current_app] SERVER_URL=$server_url ..."
+echo "Building app [$current_app]..."
+echo "---------------------------------------"
+echo "      SERVER_URL:[$server_url]"
 echo "MOBILE_BUILD_URL:[$mobile_build_url]"
-
+echo "---------------------------------------"
 # modificar archivo de cordova para aceptar certificados autofirmados
 # cd /opt/application/android/app/.meteor/local/cordova-build/platforms/android/CordovaLib/src/org/apache/cordova/engine
 # /app/.meteor/local/cordova-build/platforms/android/CordovaLib/src/org/apache/cordova/engine/SystemWebViewClient.java
