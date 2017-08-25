eaa64844aaf746d5ae186183a5d99cd942f811f2
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-25 12:20:18 -0500

    Update k-build.sh

--- scripts/k-build.sh
+++ scripts/k-build.sh
@@ -2,7 +2,7 @@
 source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
 current_app=$(kalan-var "CURRENT_APP")
 server_url="$(kalan-var 'SERVER_URL')"
-mobile_build_url="$(file_line_value /opt/application/$APP_NAME/app/app_config.txt 'MOBILE_BUILD_URL')"
+mobile_build_url="$(file_line_value /opt/application/$current_app/app/app_config.txt 'MOBILE_BUILD_URL')"
 
 echo "Building app [$current_app]..."
 echo "---------------------------------------"
