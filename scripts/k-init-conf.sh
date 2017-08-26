6bb10e7674db031888893721ae0b79bd60d68b23
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-26 11:40:06 -0500

    Create k-init-conf.sh

---
+++ scripts/k-init-conf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash  
+source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
+current_app=$(kalan-var "CURRENT_APP")
+   
+ server_url="$(kalan-var 'SERVER_URL')"
+ if [ -z "$server_url" ] ;then
+    server_url="http://$DOMAIN_NAME"
+    kalan-var "SERVER_URL" "$server_url"
+ fi
+
+ ssl_mail="$(kalan-var 'SSL_MAIL')"
+ if [ -z "$ssl_mail" ] ;then
+    ssl_mail="changeme"
+    kalan-var "SSL_MAIL" "$ssl_mail"
+ fi
+
+ ssl_domain="$(kalan-var 'SSL_DOMAIN')"
+ if [ -z "$ssl_domain" ] ;then
+    ssl_domain="changeme"
+    kalan-var "SSL_DOMAIN" "$ssl_domain"
+ fi
