df37ed79c1153924a818179eac6f2936538f3f70
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-26 01:06:23 -0500

    Update k-certbot.sh

--- scripts/k-certbot.sh
+++ scripts/k-certbot.sh
@@ -1,15 +1,19 @@
 #!/bin/bash  
-source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
-current_app=$(kalan-var "CURRENT_APP")
+if [ "$(whoami)" == "root" ];then
+  source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
+  current_app=$(kalan-var "CURRENT_APP")
 
-server_url="$(kalan-var 'SERVER_URL')"
-ssl_mail="$(kalan-var 'SSL_MAIL')"
-ssl_domain="$(kalan-var 'SSL_DOMAIN')"
+  server_url="$(kalan-var 'SERVER_URL')"
+  ssl_mail="$(kalan-var 'SSL_MAIL')"
+  ssl_domain="$(kalan-var 'SSL_DOMAIN')"
 
-if [ -z "$ssl_mail" ] || [ -z "$ssl_domain" ] || [ "$ssl_mail" == "changeme" ] || [ ! "$ssl_domain" == "changeme" ];then
-  echo "SSL certificate can not be created"
-  echo "Must define valid SSL_MAIL and SSL_DOMAIN at settings"
+  if [ -z "$ssl_mail" ] || [ -z "$ssl_domain" ] || [ "$ssl_mail" == "changeme" ] || [ ! "$ssl_domain" == "changeme" ];then
+    echo "SSL certificate can not be created"
+    echo "Must define valid SSL_MAIL and SSL_DOMAIN at settings"
+  else
+    echo "Creating SSL certificate (certbot)"
+    echo "mail:[$ssl_mail] domain:[ssl_domain]"
+  fi
 else
-  echo "Creating SSL certificate (certbot)"
-  echo "mail:[$ssl_mail] domain:[ssl_domain]"
+   echo "You must be root to create SSL certificate"
 fi
