74e08df097954af371691aa01243f040c406f46d
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2017-08-26 01:00:09 -0500

    Create k-certbot.sh

---
+++ scripts/k-certbot.sh
@@ -0,0 +1,15 @@
+#!/bin/bash  
+source $LOCAL_IMAGE_PATH/scripts/k-lib.sh
+current_app=$(kalan-var "CURRENT_APP")
+
+server_url="$(kalan-var 'SERVER_URL')"
+ssl_mail="$(kalan-var 'SSL_MAIL')"
+ssl_domain="$(kalan-var 'SSL_DOMAIN')"
+
+if [ -z "$ssl_mail" ] || [ -z "$ssl_domain" ] || [ "$ssl_mail" == "changeme" ] || [ ! "$ssl_domain" == "changeme" ];then
+  echo "SSL certificate can not be created"
+  echo "Must define valid SSL_MAIL and SSL_DOMAIN at settings"
+else
+  echo "Creating SSL certificate (certbot)"
+  echo "mail:[$ssl_mail] domain:[ssl_domain]"
+fi
